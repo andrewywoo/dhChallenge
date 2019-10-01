30da00e4c7d880d75497ab90506f0afce41d5765
Andrew Woo <andrew.woo@verizon.net>
2019-10-01 12:26:56 -0400

    Updates

---
+++ precache-manifest.2a0a849e621fb75b3c336a3b36c0802f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0cefec974adf0710c2fb3d17a8092c",
+    "url": "/dailyharvest/index.html"
+  },
+  {
+    "revision": "d06973f6d9b00f8b8085",
+    "url": "/dailyharvest/static/css/main.4bd0bf9c.chunk.css"
+  },
+  {
+    "revision": "2d5da75041be2a0f494d",
+    "url": "/dailyharvest/static/js/2.9bff33a0.chunk.js"
+  },
+  {
+    "revision": "d06973f6d9b00f8b8085",
+    "url": "/dailyharvest/static/js/main.11571e10.chunk.js"
+  },
+  {
+    "revision": "55d33caa76245af4f286",
+    "url": "/dailyharvest/static/js/runtime-main.399c1d7c.js"
+  }
+]);
